7fb93762711b265545748688aefd4c6b63e72f6e
Agustin Daniel Messina <agustindmessina@gmail.com>
2023-11-21 15:34:42 -0300

    Update layout and add Attivanse logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 export default function Home() {
 	return (
-		<main className="flex justify-center">
+		<main className="flex justify-center z-20">
 			<div className="max-w-[1920px] w-full">
-				<header className="bg-header-img w-full h-[1178px] bg-center bg-no-repeat">
-
-        </header>
+				<header className="bg-header-img w-full h-[106vh] bg-top bg-no-repeat"></header>
 			</div>
 		</main>
 	);
