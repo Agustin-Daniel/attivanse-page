bf18cbbe36255dc5568f38d63e853054be23b922
Agustin Daniel Messina <agustindmessina@gmail.com>
2023-11-23 10:36:57 -0300

    Refactor page component to use separate header and services components

---
+++ app/components/ui/Servicios.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+export const Servicios = () => {
+  return (
+    <section className="flex flex-col">
+        <h2>Servicios</h2>
+        <div>
+            <div></div>
+            
+        </div>
+    </section>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
-import Link from "next/link";
+import { Servicios } from "./components/ui/Servicios";
+import { Header } from "./components/ui/Header";
 
 export default function Home() {
 	return (
 		<main className="flex justify-center z-20">
 			<div className="max-w-[1920px] w-full">
-				<header className="bg-header-img-768 bg-top bg-no-repeat px-6 flex pt-36 w-full h-[502px] md:bg-left lg:bg-top md:bg-header-img-1920 md:px-12 lg:h-[106vh] lg:pl-28 lg:pt-0 lg:items-center">
-					<div className="flex flex-col gap-12 max-w-[440px] md:gap-8">
-						<h1 className="lg:w-[630px]">Existimos para ayudar a nuestros clientes a crecer</h1>
-						<Link role="button" className="btn-primary w-[207px] lg:w-[274px]" href="#contacto">Programa una reunión</Link>
-					</div>
-				</header>
+				<Header />
+				<Servicios />
 			</div>
 		</main>
 	);
