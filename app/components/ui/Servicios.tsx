b43cbf08c31d9b9116a825980bc64c3cdfd5d14f
Agustin Daniel Messina <agustindmessina@gmail.com>
2023-11-25 00:00:03 -0300

    Refactor UI layout in Servicios component

--- app/components/ui/Servicios.tsx
+++ app/components/ui/Servicios.tsx
@@ -4,13 +4,13 @@ import {PenSVG, CodeSVG, AddonsSVG, MegaphoneSVG, CheckSVG} from "./icons"
 export const Servicios = () => {
   return (
     <section className="flex flex-col bg-gray">
-        <h2 className="text-center mt-20 mb-8">Servicios</h2>
-        <div className="flex items-center flex-col gap-1">
-            <div className="flex gap-1 items-center"><div className="service-icon"><PenSVG /></div><h3>Diseño</h3></div>
-            <Image src="/img/design.png" width={495} height={487} alt="design image" />
-            <div className="flex flex-col gap-3">
-              <p className="w-[301px]">Lörem ipsum ser. Pokenad kvasir berat. Pseudov mobevis ponde monedossade i vusamma. Mipektig sisat fån beda, pres. Dologi bigen, plalig, besk.</p>
-              <ul className="">
+        <h2 className="text-center mt-20">Servicios</h2>
+        <div className="flex items-center flex-col px-8 pb-16 pt-11">
+            <div className="flex gap-1 items-center mb-8"><div className="service-icon"><PenSVG /></div><h3>Diseño</h3></div>
+            <Image className="w-[301px] mb-12" src="/img/design.png" width={495} height={487} alt="design image" />
+            <div className="flex flex-col ">
+              <p className="w-[301px] mb-10">Lörem ipsum ser. Pokenad kvasir berat. Pseudov mobevis ponde monedossade i vusamma. Mipektig sisat fån beda, pres. Dologi bigen, plalig, besk.</p>
+              <ul className="flex flex-col gap-4">
                 <li className="flex gap-1"><CheckSVG height={24} width={24} />Diseñador experto dedicado</li>
                 <li className="flex gap-1"><CheckSVG height={24} width={24} />Diseño acorde a su marca</li>
                 <li className="flex gap-1"><CheckSVG height={24} width={24} />Estudio de la competencia</li>
@@ -20,7 +20,7 @@ export const Servicios = () => {
         </div>
         <div>
             <div><div className="service-icon"><CodeSVG /></div><h3>Tecnologia</h3></div>
-            <Image src="/img/technology.png" width={611} height={487} alt="technology image" />
+            <Image className="w-[368px]" src="/img/technology.png" width={611} height={487} alt="technology image" />
             <p>Lörem ipsum ser. Pokenad kvasir berat. Pseudov mobevis ponde monedossade i vusamma. Mipektig sisat fån beda, pres. Dologi bigen, plalig, besk.</p>
             <ul>
               <li><CheckSVG height={24} width={24} />Sitio web ultra rápido</li>
@@ -31,7 +31,7 @@ export const Servicios = () => {
         </div>
         <div>
             <div><div className="service-icon"><AddonsSVG /></div><h3>Agregados</h3></div>
-            <Image src="/img/addons.png" width={504} height={519} alt="addons image" />
+            <Image className="w-[297px]" src="/img/addons.png" width={504} height={519} alt="addons image" />
             <p>Lörem ipsum ser. Pokenad kvasir berat. Pseudov mobevis ponde monedossade i vusamma. Mipektig sisat fån beda, pres. Dologi bigen, plalig, besk.</p>
             <ul>
               <li><CheckSVG height={24} width={24} />ADA (Web inclusiva)</li>
@@ -42,7 +42,7 @@ export const Servicios = () => {
         </div>
         <div>
             <div><div className="service-icon"><MegaphoneSVG /></div><h3>Marketing</h3></div>
-            <Image src="/img/marketing.png" width={561} height={489} alt="marketing image" />
+            <Image className="w-[355px]" src="/img/marketing.png" width={561} height={489} alt="marketing image" />
             <p>Lörem ipsum ser. Pokenad kvasir berat. Pseudov mobevis ponde monedossade i vusamma. Mipektig sisat fån beda, pres. Dologi bigen, plalig, besk.</p>
             <ul>
               <li><CheckSVG height={24} width={24} />Lorem ipsum</li>
