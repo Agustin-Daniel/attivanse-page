87da879b68356dd496a47b50d4fffda3ebae8ee6
Agustin Daniel Messina <agustindmessina@gmail.com>
2023-11-25 09:37:07 -0300

    Refactor UI layout in Servicios component

--- app/components/ui/Servicios.tsx
+++ app/components/ui/Servicios.tsx
@@ -3,14 +3,14 @@ import {PenSVG, CodeSVG, AddonsSVG, MegaphoneSVG, CheckSVG} from "./icons"
 
 export const Servicios = () => {
   return (
-    <section className="flex flex-col bg-gray">
+    <section className="flex flex-col bg-gray lg:items-center">
         <h2 className="text-center mt-20">Servicios</h2>
-        <div className="flex items-center flex-col px-8 pb-16 pt-11">
-            <div className="flex gap-1 items-center mb-8"><div className="service-icon"><PenSVG /></div><h3>Diseño</h3></div>
-            <Image className="w-[301px] mb-12" src="/img/design.png" width={495} height={487} alt="design image" />
-            <div className="flex flex-col ">
+        <div className="flex items-center flex-col px-8 pb-16 pt-11 lg:grid lg:justify-center lg:px-[7rem] lg:grid-cols-2 lg:max-w-[1200px] lg:w-full">
+            <div className="flex gap-1 items-center mb-8 lg:self-end"><div className="service-icon"><PenSVG /></div><h3>Diseño</h3></div>
+            <Image className="w-[301px] mb-12 lg:row-span-2 lg:mb-0 lg:w-[42vw] lg:max-w-[495px] lg:self-center" src="/img/design.png" width={495} height={487} alt="design image" />
+            <div className="flex flex-col lg:self-start w-fit">
               <p className="w-[301px] mb-10">Lörem ipsum ser. Pokenad kvasir berat. Pseudov mobevis ponde monedossade i vusamma. Mipektig sisat fån beda, pres. Dologi bigen, plalig, besk.</p>
-              <ul className="flex flex-col gap-4">
+              <ul className="flex flex-col gap-4 w-fit">
                 <li className="flex gap-1"><CheckSVG height={24} width={24} />Diseñador experto dedicado</li>
                 <li className="flex gap-1"><CheckSVG height={24} width={24} />Diseño acorde a su marca</li>
                 <li className="flex gap-1"><CheckSVG height={24} width={24} />Estudio de la competencia</li>
