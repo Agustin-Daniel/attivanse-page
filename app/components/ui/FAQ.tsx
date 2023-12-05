463b233b9012a01772b5e5114ea759b040360d75
Agustin Daniel Messina <agustindmessina@gmail.com>
2023-12-05 19:27:24 -0300

    Add Accordion component to FAQ page

--- app/components/ui/FAQ.tsx
+++ app/components/ui/FAQ.tsx
@@ -1,9 +1,11 @@
+import { Accordion } from "./Accordion"
 
 
 export const FAQ = () => {
   return (
     <div className="bg-black py-12 px-5 flex flex-col items-center">
       <h2>Preguntas frecuentes</h2>
+      <Accordion />
     </div>
   )
 }
