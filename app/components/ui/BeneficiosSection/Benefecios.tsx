2287ba7967ef5b14955afd25b418082467477168
Agustin Daniel Messina <agustindmessina@gmail.com>
2023-12-30 17:33:00 -0300

    Update UI text and card titles

--- app/components/ui/BeneficiosSection/Benefecios.tsx
+++ app/components/ui/BeneficiosSection/Benefecios.tsx
@@ -10,7 +10,7 @@ export const Benefecios = () => {
 			<div className="w-[90%] max-w-[1450px] pb-24 mx-auto">
 				<div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
 					<h2 className="mb-2 lg:lg:text-5xl font-bold">
-						<strong className="text-[#FEBD29]">Beneficios</strong> de trabajar con nosotros
+						<strong className="text-[#FEBD29]">Beneficios</strong> de trabajar con los mejores
 					</h2>
 					<p className="lg:w-1/2 w-full leading-relaxed">
                         Su empresa se beneficiará de nuestra experiencia en el desarrollo de software
--- app/components/ui/BeneficiosSection/SwiperBeneficios.tsx
+++ app/components/ui/BeneficiosSection/SwiperBeneficios.tsx
@@ -8,45 +8,45 @@ import { Autoplay } from 'swiper/modules';
 
 const cards = [
     {
-        title: "Capture clientes potenciales",
+        title: "Diseño de Primer Orden",
         description: "Atraiga más clientes con un hermoso sitio web que siga los valores de su marca.",
-        image: "image"
+        // image: "image"
     },
     {
-        title: "Capture clientes potenciales",
-        description: "Atraiga más clientes con un hermoso sitio web que siga los valores de su marca.",
-        image: "image"
+        title: "Posicionamiento Internacional",
+        description: "Cierre ventas en el exterior siendo visible en todos los buscadores del mundo.",
+        // image: "image"
     },
     {
-        title: "Capture clientes potenciales",
-        description: "Atraiga más clientes con un hermoso sitio web que siga los valores de su marca.",
-        image: "image"
+        title: "Todo es Programable",
+        description: "Todo lo que mostramos es apenas un poco de lo que podesmos hacer, el cielo es el limite.",
+        // image: "image"
     },
     {
-        title: "Capture clientes potenciales",
-        description: "Atraiga más clientes con un hermoso sitio web que siga los valores de su marca.",
-        image: "image"
+        title: "Seguridad ante Todo",
+        description: "Impida ataques de DOSS, spam y de haking con nuestra arquitectura.",
+        // image: "image"
     },
     {
-        title: "Capture clientes potenciales",
-        description: "Atraiga más clientes con un hermoso sitio web que siga los valores de su marca.",
-        image: "image"
+        title: "Venda mas Rapido",
+        description: "Agilice las ventas de sus productos con un sistema de venta personalizado.",
+        // image: "image"
     },
     {
-        title: "Capture clientes potenciales",
-        description: "Atraiga más clientes con un hermoso sitio web que siga los valores de su marca.",
-        image: "image"
+        title: "Presupuestos a un Click",
+        description: "Emita presupuestos de manera automática directamente desde su web.",
+        // image: "image"
     },
     {
-        title: "Capture clientes potenciales",
-        description: "Atraiga más clientes con un hermoso sitio web que siga los valores de su marca.",
-        image: "image"
+        title: "Blogs Personalizados",
+        description: "Genere valor agregado a su marca con un blog totalmente personalizado.",
+        // image: "image"
     },
     {
-        title: "Capture clientes potenciales",
-        description: "Atraiga más clientes con un hermoso sitio web que siga los valores de su marca.",
-        image: "image"
-    },
+        title: "Sofware a Medida",
+        description: "Podemos crear literalmente LO QUE SEA, hasta si lo dea una pagina como Netflix.",
+        // image: "image"
+    }, 
 ]
 
 
