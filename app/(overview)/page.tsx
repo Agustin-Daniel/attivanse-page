b4b929cf0ffc996191c7c133b6fe400b4ab965bd
Agustin Daniel Messina <agustindmessina@gmail.com>
2024-01-24 19:28:43 -0300

    Add metadata to page.tsx and remove unused code

--- app/(overview)/page.tsx
+++ app/(overview)/page.tsx
@@ -7,6 +7,27 @@ import { Contact } from "../components/ui/Contact";
 import { FAQ } from "../components/ui/FAQ";
 import { Benefecios } from "../components/ui/BeneficiosSection/Benefecios";
 import { Tools } from "../components/ui/ToolsSection/Tools";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+	title: 'Attivanse | Diseño web para Empresas',
+	description: "Potencia el rendimiento de tu empresa con nuestro servicio de diseño web y desarrollo de aplicaciones para industrias y fábricas.Expertos en SEO, creamos soluciones digitales personalizadas para impulsar tu visibilidad online, aumentar la productividad y fortalecer tu presencia en el mercado. ¡Descubre cómo optimizar tu presencia digital con nosotros!",
+	keywords: "diseño web, diseño web para empresas, diseño web para industrias, diseño web para fábricas, diseño web para pymes, diseño web para emprendedores, diseño web para negocios, diseño web para comercios, diseño web para tiendas online, diseño web para ecommerce, diseño web para profesionales, diseño web para servicios",
+	openGraph: {
+	  title: 'Attivanse | Diseño web para Empresas',
+	  description: "Potencia el rendimiento de tu empresa con nuestro servicio de diseño web y desarrollo de aplicaciones!",
+	  images: [
+		{
+		  url: "/img/attivanse-og.png",
+		  width: 601,
+		  height: 301,
+		  alt: "attivanse-og-image",
+		}
+	  ],
+	  type: 'website',
+	},
+  }
 
 
 export default function Home() {
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from 'next'
 import { Navbar } from './components/ui/Navbar'
 import './globals.css'
 import { manrope } from './components/ui/fonts'
@@ -6,27 +5,6 @@ import { Footer } from './components/ui/Footer'
 import { WhatsAppButton } from './components/ui/atoms/WhatsAppButton'
 import { ScrollProvider } from "@/app/context/ScrollContext";
 
-
-
-
-export const metadata: Metadata = {
-  title: 'Attivanse | Diseño web para Empresas',
-  description: "Potencia el rendimiento de tu empresa con nuestro servicio de diseño web y desarrollo de aplicaciones para industrias y fábricas.Expertos en SEO, creamos soluciones digitales personalizadas para impulsar tu visibilidad online, aumentar la productividad y fortalecer tu presencia en el mercado. ¡Descubre cómo optimizar tu presencia digital con nosotros!",
-  keywords: "diseño web, diseño web para empresas, diseño web para industrias, diseño web para fábricas, diseño web para pymes, diseño web para emprendedores, diseño web para negocios, diseño web para comercios, diseño web para tiendas online, diseño web para ecommerce, diseño web para profesionales, diseño web para servicios",
-  openGraph: {
-    title: 'Attivanse | Diseño web para Empresas',
-    description: "Potencia el rendimiento de tu empresa con nuestro servicio de diseño web y desarrollo de aplicaciones!",
-    images: [
-      {
-        url: "/img/attivanse-og.png",
-        width: 601,
-        height: 301,
-        alt: "attivanse-og-image",
-      }
-    ],
-    type: 'website',
-  },
-}
 
 export default function RootLayout({
   children,
