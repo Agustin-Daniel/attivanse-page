c29f43c03b999a371ab0bb69bfae585add565c18
Agustin Daniel Messina <agustindmessina@gmail.com>
2024-03-28 22:38:42 -0300

    imp

--- app/(overview)/page.tsx
+++ app/(overview)/page.tsx
@@ -1,8 +1,8 @@
 import { Servicios } from "../components/ui/Servicios";
 import { Header } from "../components/ui/Header";
-import { Metodologia } from "../components/ui/MetodologiaSection/Metodologia";
+// import { Metodologia } from "../components/ui/MetodologiaSection/Metodologia";
 import { CasoDeEstudio } from "../components/ui/CasoDeEstudio";
-import { Pricing } from "../components/ui/Pricing";
+// import { Pricing } from "../components/ui/Pricing";
 import { Contact } from "../components/ui/Contact";
 import { FAQ } from "../components/ui/FAQ";
 import { Benefecios } from "../components/ui/BeneficiosSection/Benefecios";
